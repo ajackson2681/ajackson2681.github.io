09c525350d726f7f0ba0d14a8e12f5d6f1284859
Adam Jackson <ajackson2681@gmail.com>
2022-01-13 21:06:13 -0600

    Retrieves repo variable for version dynamically
    This allows the latest version of the dmtool to be displayed with the
    entry on the projects page

---
+++ projects.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+
+    fetch("https://api.bitbucket.org/2.0/repositories/ajackson2681/dmtools/pipelines_config/variables/")
+        .then( (value) => value.json() )
+        .then( (data) => {
+            const version = data.values.filter( (value) => value.key === "LATEST_VERSION")[0]?.value;
+            const versionNode = document.getElementById("version");
+            versionNode.innerText = `(v${version})`
+        });
+}

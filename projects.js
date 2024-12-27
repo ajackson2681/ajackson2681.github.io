fffd02064ed5d7d5950d497e83a0d4588a2d7d3d
Adam Jackson <ajackson2681@gmail.com>
2024-12-27 12:29:54 -0600

    Different stylesheets and toggleable theme

--- projects.js
+++ projects.js
@@ -1,5 +1,37 @@
-window.onload = () => {
+function loadColors() {
+    document.head.removeChild(document.head.getElementsByTagName("link")[0]);
+    console.log('load color', localStorage.getItem("theme"));
+    if (localStorage.getItem("theme") == "dark") {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = "index-dark.css";
+        document.head.appendChild(link);
+    }
+    else {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = "index.css";
+        document.head.appendChild(link);
+    }
+}
 
+function setColor() {
+    if (localStorage.getItem("theme") == "dark") {
+        localStorage.setItem("theme", "light");
+    }
+    else {
+        localStorage.setItem("theme", "dark");
+    }
+
+    loadColors();
+}
+
+
+window.onload = () => {
+    loadColors();
+    
     fetch("https://api.bitbucket.org/2.0/repositories/ajackson2681/dmtools/pipelines_config/variables/")
         .then( (value) => value.json() )
         .then( (data) => {
@@ -7,4 +39,6 @@ window.onload = () => {
             const versionNode = document.getElementById("version");
             versionNode.innerText += ` (v${version})`;
         });
+
+
 }
